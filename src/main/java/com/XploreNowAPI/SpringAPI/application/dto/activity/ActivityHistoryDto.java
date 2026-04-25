cb170cfe3f07fa09aeeb0a20037ef0ddbabe6de6
fquinonez <fquinonez@uade.edu.ar>
2026-04-25 18:11:27 -0300

    DTO para el listado de actividades

---
+++ src/main/java/com/XploreNowAPI/SpringAPI/application/dto/activity/ActivityHistoryDto.java
@@ -0,0 +1,13 @@
+package com.XploreNowAPI.SpringAPI.application.dto.activity;
+
+import java.time.LocalDate;
+
+public record ActivityHistoryDto(
+        Long activityId,
+        String name,
+        LocalDate date,
+        String destination,
+        String guideName,
+        Integer durationHours,
+        Integer rating
+) {}
